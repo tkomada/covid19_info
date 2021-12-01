5aa02d8585c441b4c8874e67d8b3c719b512fd31
tkomada <komada.toshihiko@nihon-u.ac.jp>
2021-12-01 22:07:44 +0900

    Add files via upload
    不自然な読みを改訂

--- 2021-12-01--Omicron/scripts/layout.js
+++ 2021-12-01--Omicron/scripts/layout.js
@@ -1,3 +1,3 @@
-var strDefaultLayout = '[[2,1,"P"],[3,1,"P"],[3,29,"P"],[3,54,"P"],[3,144,"P"],[3,147,"P"],[3,218,"P"],[4,1,"P"]]' ;
+var strDefaultLayout = '[[2,1,"P"],[3,1,"P"],[4,1,"P"]]' ;
 
 var bVerticalMode = false ;
